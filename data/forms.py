9a6661911dc794d9005fa5582ab9b840f7c63fe0
Stealth298730 <serhii.lundiak@gmail.com>
2024-12-26 19:50:27 +0200

    1

--- data/forms.py
+++ data/forms.py
@@ -4,9 +4,9 @@ class SignupForm(FlaskForm):
     username = wtforms.StringField("Введіть логін", validators = [wtforms.validators.DataRequired()])
     email = wtforms.EmailField("Введіть свою електрону пошту", validators = [wtforms.validators.DataRequired(),wtforms.validators.Email()])
     password = wtforms.PasswordField("Пароль", validators = [wtforms.validators.DataRequired(),wtforms.validators.length(8)])
-    sudmit = wtforms.SubmitField("Зареєструватись")
+    submit = wtforms.SubmitField("Зареєструватись")
 
 class LoginForm(FlaskForm):
     username = wtforms.StringField("Введіть логін або електронну адресу", validators = [wtforms.validators.DataRequired()])
     password = wtforms.PasswordField("Пароль", validators = [wtforms.validators.DataRequired()])
-    sudmit = wtforms.SubmitField("Зареєструватись")
+    submit = wtforms.SubmitField("Зареєструватись")
